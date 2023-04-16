18d5d4ed544db7d2db42fae8911f6fa74689757d
Takafumi Masuno <95342551+takafumi-masuno@users.noreply.github.com>
2023-04-16 14:07:22 +0900

    chore: edit yml file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  basePath: process.env.GITHUB_ACTIONS && "/gum-talk-app",
+  trailingSlash: true,
+};
