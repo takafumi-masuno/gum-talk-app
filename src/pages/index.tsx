06e1fea4dcdc1a0d144a23b9ba1a174887ed11c4
lak-masuno <masuno-ta@lakeel.com>
2023-08-08 15:50:22 +0900

    feat: トークテーマ取得APIの実装
    Update .gitignore
    
    Delete team-provider-info.json
    
    feat: Amplifyの環境構築

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { API } from '@aws-amplify/api'
+import config from '../aws-exports'
+import { getTalkTheme } from "@/graphql/queries";
+import { GetTalkThemeQuery, talkTheme } from "@/API";
+import { GraphQLResult } from '@aws-amplify/api-graphql'
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+API.configure(config)
 
-export default function Home() {
+const getTT = async () => {
+  const id = Math.floor(Math.random() * 6) + 1;
+  const response = await API.graphql({
+     query: getTalkTheme,
+     variables: {
+        talkId: id
+     },
+  }) as GraphQLResult<GetTalkThemeQuery>
+  return response
+}
+
+const Home = () => {
+  const [talkTheme, setTalkTheme] = useState('');
+  useEffect(() => {
+    const talkThemeResponse = getTT();
+    talkThemeResponse.then(({data}) => setTalkTheme(data!.getTalkTheme!.talkTheme))
+  }, [])
   return (
     <>
       <Head>
@@ -13,7 +33,15 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>Gum Talk App</h1>
+        <div className="mt-5 mb-8 first:mt-0 last:mb-0 relative overflow-hidden rounded-2xl">
+          <div className="pt-2 bg-slate-800 shadow-lg group w-48">
+            <div className="text-sky-300 border-t border-b border-t-transparent border-b-sky-300 px-4 py-1 text-center h-12">トークテーマ</div>
+            <div className="children:!shadow-none children:bg-transparent text-center align-middle h-24">{talkTheme}について</div>
+          </div>
+        </div>
       </main>
     </>
   );
 }
+
+export default Home;
